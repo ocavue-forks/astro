7773be80ce43cb7c7d67993bebf825b77b89b079
ocavue <ocavue@gmail.com>
2026-04-27 23:36:58 +1000

    w

---
+++ rmcache.sh
@@ -0,0 +1,13 @@
+function rmcache {
+  find . \( \
+    -name ".eslintcache" -o \
+    -name ".next" -o \
+    -name ".swc" -o \
+    -name ".turbo" -o \
+    -name ".mypy_cache" -o \
+    -name ".svelte2tsx-language-server-files" -o \
+    -path "*/node_modules/.vite" -o \
+    -path "*/node_modules/.cache" -o \
+    -path "*/node_modules/.astro" \
+  \) -prune -print -exec rm -rf {} +
+}
